b912c503e4360dd950b2a3548b81a5d7b15607fa
Majid Aziz <majidaziz7ctech@gmail.com>
2023-03-28 22:07:10 +0500

    added search functionality

--- src/Components/productlist/ProductList.tsx
+++ src/Components/productlist/ProductList.tsx
@@ -1,60 +1,138 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { products } from '../../utils';
 import { ProductProps } from '../products/Products';
 import './productList.scss';
 import Product from '../product/Product';
 import Navbar from '../navbar/Navbar';
 import ReactPaginate from 'react-paginate';
-
-
+import { lists } from '../../lists';
+import axios from 'axios';
+import { useLocation } from 'react-router-dom';
+import { Search, SearchOffOutlined } from '@mui/icons-material';
 
 
 const ProductList = () => {
     const [data, setData] = useState<ProductProps[]>([])
 
+    const [category, setCategory] = useState<ProductProps[]>([])
+    const location = useLocation()
+    const [inputSearch, setInputSearch] = useState('')
+    const [searchItem, setSearchItem] = useState<ProductProps[]>([])
+    const inputRef = useRef<HTMLInputElement>(null)
+
+
+
     const [itemOffset, setItemOffset] = useState(0);
-    const itemsPerPage = 10;
+    const itemsPerPage = 9;
     const endOffset = itemOffset + itemsPerPage;
     const currentItems = data.slice(itemOffset, endOffset);
     const pageCount = Math.ceil(data.length / itemsPerPage);
+
 
     const handlePageClick = (event: any) => {
         const newOffset = (event.selected * itemsPerPage) % data.length;
         setItemOffset(newOffset);
     };
 
+    const handle = async (id: string) => {
+        const res = await axios.get(`https://dummyjson.com/products/category/${id}`)
+        setCategory(res.data.products)
+    }
+
+    // const handleSearch = () => {
+    //     if (inputRef.current) {
+    //         setInputSearch(inputRef.current.value.toLowerCase());
+    //     }
+    //     const filteredData = data.filter((item: ProductProps) => {
+    //         return item.title.toLowerCase().includes(inputSearch);
+    //     });
+
+    //     setSearchItem(filteredData);
+    // };
+
+
+const handleSearch = (e:React.FormEvent) => {
+    e.preventDefault()
+    if (inputRef.current) {
+                setInputSearch(inputRef.current.value.toLowerCase());
+            }
+            const filteredData = data.filter((item) => {
+                return item.title.toLowerCase().includes(inputSearch);
+            });
+            setSearchItem(filteredData);
+}
+
+
     useEffect(() => {
         products().then(res => setData(res))
     }, [])
+
+    console.log("searchItem", searchItem)
+    console.log("Input value", inputSearch)
 
     return (
         <>
             <Navbar />
             <div className="productsHolder">
                 <div className="left">
-                    left
+                    <ul>
+                        <li>View All</li>
+                        {
+                            lists.map((item) => (
+                                <>
+                                    <li onClick={() => handle(item)}>{item}</li>
+                                </>
+                            ))
+                        }
+                    </ul>
                 </div>
                 <div className="right">
-                <div className='list'>
-                    {
-                        currentItems.map((item) => (
-                            <Product items={item} />
-                        ))
-                    }
-                </div>
-                <ReactPaginate
-                    breakLabel="..."
-                    nextLabel="next >"
-                    onPageChange={handlePageClick}
-                    pageRangeDisplayed={5}
-                    pageCount={pageCount}
-                    previousLabel="< previous"
-                    containerClassName='pagination'
-                    pageClassName='page-num'
-                    previousLinkClassName='previous'
-                    nextLinkClassName='previous'
-                    activeLinkClassName='active'
-                />
+                    <form className="search" onSubmit={handleSearch}>
+                        <input type="text" placeholder='Search product...' ref={inputRef} />
+                        <button className='btn'>
+                            <Search className='sear' />
+                        </button>
+                    </form>
+                    <div className='list'>
+                        {
+                            category.length > 0
+                                ?
+                                (
+                                    category.map((item) => (
+                                        <>
+                                            <Product items={item} />
+                                        </>
+                                    ))
+                                )
+                                :
+                                searchItem.length > 0
+                                    ?
+                                    searchItem.map((item) => (
+                                        <>
+                                            <Product items={item} />
+                                        </>
+                                    ))
+                                    :
+                                    (
+                                        currentItems.map((item) => (
+                                            <Product items={item} />
+                                        ))
+                                    )
+                        }
+                    </div>
+                    <ReactPaginate
+                        breakLabel="..."
+                        nextLabel="next >"
+                        onPageChange={handlePageClick}
+                        pageRangeDisplayed={5}
+                        pageCount={pageCount}
+                        previousLabel="< previous"
+                        containerClassName='pagination'
+                        pageClassName='page-num'
+                        previousLinkClassName='previous'
+                        nextLinkClassName='previous'
+                        activeLinkClassName='active'
+                    />
                 </div>
             </div>
         </>
---
+++ src/lists.tsx
@@ -0,0 +1,9 @@
+export const lists = [
+    'smartphones',
+    'fragrances',
+    'skincare',
+    'groceries',
+    'sunglasses',
+    'automotive',
+    'mens-shirts',
+]
