b970b5257e341c0b386b20c6e040d0ad3f3f3d9e
Majid Aziz <majidaziz7ctech@gmail.com>
2023-03-28 11:24:27 +0500

    login updated

--- src/Components/login/Login.tsx
+++ src/Components/login/Login.tsx
@@ -4,14 +4,18 @@ import { useAppDispatch, useAppSelector } from '../../redux/hooks';
 import './login.scss';
 import { success } from '../../redux/slices/userSlice';
 import { useNavigate } from 'react-router-dom';
+import { VisibilityOffOutlined } from '@mui/icons-material';
+import { Button } from '@mui/material';
 
 
 const Login = () => {
 
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
+    const [show, setShow] = useState(false)
     const router = useNavigate()
     const dispatch = useAppDispatch()
+
 
     const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault()
@@ -33,11 +37,16 @@ const Login = () => {
     return (
         <div className='login'>
             <form onSubmit={handleSubmit}>
-                <div>
+                <div className="holderForm">
                     <h1> Login </h1>
                     <input type="text" placeholder='Enter username' value={username} onChange={(e) => setUsername(e.target.value)} />
-                    <input type="password" placeholder='Enter Password' value={password} onChange={(e) => setPassword(e.target.value)} />
-                    <button type='submit'>Login</button>
+                    <div className="holderInput">
+                        <input type={show ? "text" : "password"} placeholder='Enter Password' value={password} onChange={(e) => setPassword(e.target.value)} />
+                        <VisibilityOffOutlined className='ico' onClick={() => setShow(!show)} />
+                    </div>
+                    <div>
+                        <Button type='submit' variant='contained' size='small' className='btn' >Login</Button>
+                    </div>
                 </div>
             </form>
         </div>
